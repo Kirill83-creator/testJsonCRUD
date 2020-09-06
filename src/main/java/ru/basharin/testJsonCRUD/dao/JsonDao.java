1643dec82a610967276c0d3b058f83cbdb627742
drbah <dr.bahahza@yandex.ru>
2020-09-06 12:24:34 +0300

    delete daoImpl

--- src/main/java/ru/basharin/testJsonCRUD/dao/JsonDao.java
+++ src/main/java/ru/basharin/testJsonCRUD/dao/JsonDao.java
@@ -1,12 +1,18 @@
 package ru.basharin.testJsonCRUD.dao;
 
 import org.json.JSONObject;
+import org.springframework.data.repository.CrudRepository;
+import ru.basharin.testJsonCRUD.model.JsonObject;
 
 import java.util.List;
 
-public interface JsonDao {
+public interface JsonDao extends CrudRepository<JsonObject, Integer> {
+
     List<JSONObject> getAllJson();
+
     JSONObject getJsonById(int id);
+
     void putJson(JSONObject json);
+
     List<JSONObject> convertToJson(List<String> strings);
 }
