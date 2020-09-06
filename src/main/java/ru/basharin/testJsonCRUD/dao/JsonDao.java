f6fc360b6b3d5743a94d04c3d55590f5cf2ff275
drbah <dr.bahahza@yandex.ru>
2020-09-06 20:16:24 +0300

    fix some problem

--- src/main/java/ru/basharin/testJsonCRUD/dao/JsonDao.java
+++ src/main/java/ru/basharin/testJsonCRUD/dao/JsonDao.java
@@ -1,14 +1,13 @@
 package ru.basharin.testJsonCRUD.dao;
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.QueryByExampleExecutor;
 import org.springframework.stereotype.Repository;
 import ru.basharin.testJsonCRUD.model.JsonObject;
 
 import java.util.List;
 
 @Repository
-public interface JsonDao extends CrudRepository<JsonObject, Integer>, QueryByExampleExecutor<JsonObject> {
+public interface JsonDao extends CrudRepository<JsonObject, Integer>{
 
     List<JsonObject> findAll();
 
